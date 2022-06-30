272fe6120533a3fb78aca6743c336b11dd481e71
Jordy <yansyahjordy@gmail.com>
2022-06-30 09:09:11 +0700

    finished grid slider

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const container = document.getElementById("container")
-let size = 10;
+let size = document.getElementById("size")
+let sizeLabel = document.getElementById("sizeLabel")
+sizeLabel.textContent = `${size.value} X ${size.value}`;
 
-for (let i = 0; i < size; i++){
-    let row = document.createElement('div')
-    row.classList.add('row')
-    for (let j = 0; j < size; j++){
-        let column = document.createElement('div')
-        column.classList.add('column')
-        row.append(column);
-    }
-    container.append(row)
-    
+function slide() {
+    sizeLabel.textContent = `${size.value} X ${size.value}`;
+    let row = container.querySelectorAll(".row");
+    row.forEach(e => {
+        container.removeChild(e)
+    });
+    console.log(row)
+    makeGrid(size.value)
 }
+
+makeGrid(size.value)
+function makeGrid (gridSize){
+    for (let i = 0; i < gridSize; i++){
+        let row = document.createElement('div')
+        row.classList.add('row')
+        for (let j = 0; j < gridSize; j++){
+            let column = document.createElement('div')
+            column.classList.add('column')
+            row.append(column);
+        }
+        container.append(row)
+    }
+}
+    
